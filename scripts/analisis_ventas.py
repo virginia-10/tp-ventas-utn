d55e85d6c99b633c721022e03122fc899e9408bf
virginia-10 <mvmanchini@hotmail.com>
2026-05-13 19:56:29 -0300

    TV-2: agregar análisis básico de ventas

---
+++ scripts/analisis_ventas.py
@@ -0,0 +1,16 @@
+#Productos
+productos = ["Mouse", "Teclado", "Monitor"`]
+
+#Ventas
+ventas = [10, 5, 3]
+
+#Ventas totales
+total = sum(ventas)
+
+#Producto mas vendido
+mayor = max(ventas)
+indice = ventas.index(mayor)
+
+"Mostrar resultados
+print("Ventas totales: ", total)
+print("Producto mas vendido: ", producto[indice])
